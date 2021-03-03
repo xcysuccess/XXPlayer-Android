54740b1fbd88f74a7021bd3f735249ec160f2c54
tomxiang <30513207@qq.com>
2021-03-03 13:32:26 +0800

    增加toolbar以及toolbar上的menu

--- app/src/main/java/com/tencent/xxplayer/ui/activity/MainActivity.kt
+++ app/src/main/java/com/tencent/xxplayer/ui/activity/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.tencent.xxplayer.ui.activity
 
 import android.os.Bundle
+import android.widget.Toolbar
 import com.tencent.xxplayer.R
 import com.tencent.xxplayer.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,11 +13,19 @@ class MainActivity : BaseActivity() {
 
     override fun initView() {
         setContentView(R.layout.activity_main)
+        initToolbar()
     }
 
     override fun initData() {
+
     }
 
     override fun initListener() {
+    }
+
+    private fun initToolbar() {
+        //https://www.aidemx.cn/1051.html
+        toolbar.title = "自研XXPlayer"
+        toolbar.inflateMenu(R.menu.menu_toolbar)
     }
 }
