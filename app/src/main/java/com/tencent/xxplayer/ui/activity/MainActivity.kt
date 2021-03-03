8a37163e471ab32aa50ececdea788c1ecbd884d8
tomxiang <30513207@qq.com>
2021-03-03 16:54:18 +0800

    增加toolbar上的点击事件

--- app/src/main/java/com/tencent/xxplayer/ui/activity/MainActivity.kt
+++ app/src/main/java/com/tencent/xxplayer/ui/activity/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.tencent.xxplayer.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.View
 import android.widget.Toolbar
 import com.tencent.xxplayer.R
 import com.tencent.xxplayer.base.BaseActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.logging.Logger
+
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,5 +31,16 @@ class MainActivity : BaseActivity() {
         //https://www.aidemx.cn/1051.html
         toolbar.title = "自研XXPlayer"
         toolbar.inflateMenu(R.menu.menu_toolbar)
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId) {
+                R.id.menu_setting -> {
+                    println("1. println ")
+                }
+                R.id.menu_about -> {
+                    println("2. println ")
+                }
+            }
+            true
+        }
     }
 }
